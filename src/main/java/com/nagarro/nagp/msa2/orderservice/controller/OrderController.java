22cfb587c5f6a0da48ca1af691fe0c567071c426
Divesh Jethani <divesh.jethani@nagarro.com>
2020-03-30 01:00:31 +0530

    interface updates

--- src/main/java/com/nagarro/nagp/msa2/orderservice/controller/OrderController.java
+++ src/main/java/com/nagarro/nagp/msa2/orderservice/controller/OrderController.java
@@ -1,7 +1,5 @@
 package com.nagarro.nagp.msa2.orderservice.controller;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.nagarro.nagp.msa2.orderservice.entity.Order;
+import com.nagarro.nagp.msa2.orderservice.entity.OrderList;
 import com.nagarro.nagp.msa2.orderservice.service.OrderService;
 
 @RestController
@@ -23,8 +21,10 @@ public class OrderController {
 	OrderService orderService;
 
 	@GetMapping("/{id}")
-	public List<Order> getOrdersForUser(@PathVariable("id") int id) {
+	public OrderList getOrdersForUser(@PathVariable("id") int id) {
+		OrderList orderList = new OrderList();
 		LOGGER.info("Get Orders for User request received for user id: {}", id);
-		return orderService.getOrdersByUserId(id);
+		orderList.setOrders(orderService.getOrdersByUserId(id));
+		return orderList;
 	}
 }
